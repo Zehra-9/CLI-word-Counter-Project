bfd01b21544f924c6d1a0d9583592618ebc4e239
Zehra-9 <159943680+Zehra-9@users.noreply.github.com>
2024-04-18 23:53:16 +0500

    Add files via upload

---
+++ word.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const userAns = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }
+]);
+const words = userAns.sentence.trim().split(" ");
+console.log(words);
+console.log(`your sentence word count is ${words.length}`);
